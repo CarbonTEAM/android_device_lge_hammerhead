fc48474866863c1f3e3cc40ff2e350f35513e673
STELIX <ssspinni@gmail.com>
2014-11-24 21:32:00 +0100

    Hammerhead: Update build fingerprint and description to KTU84M
    OriginalChange-Id: Ib7779d9d0f7ba9ba91f4560e1d3c3b673b543fb0
    
    Change-Id: Ied21ee101e51587e52c17c73c815173a2b9ceff7
    Signed-off-by: STELIX <ssspinni@gmail.com>

--- carbon.mk
+++ carbon.mk
@@ -1,3 +1,18 @@
+# Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2014 The Carbon Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
@@ -20,5 +35,5 @@ PRODUCT_MANUFACTURER := LGE
 CARBON_BUILDTYPE := NIGHTLY
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-            PRODUCT_NAME=hammerhead BUILD_FINGERPRINT=google/hammerhead/hammerhead:4.4.4/KTU84P/1227136:user/release-keys \
-            PRIVATE_BUILD_DESC="hammerhead-user 4.4.4 KTU84P 1227136 release-keys"
+            PRODUCT_NAME=hammerhead BUILD_FINGERPRINT=google/hammerhead/hammerhead:5.0/LRX21O/1570415:user/release-keys \
+            PRIVATE_BUILD_DESC="hammerhead-user 5.0 LRX21O 1570415 release-keys"
