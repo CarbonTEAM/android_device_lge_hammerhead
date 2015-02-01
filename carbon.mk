e925bfe1ee3740351279e8e49e56c0f677d1ad6e
STELIX <ssspinni@gmail.com>
2015-02-01 12:37:19 +0100

    HammerHead - Change vendor path
    Change-Id: I4e413b79603f88303d72df381bcaef4a54663124
    Signed-off-by: STELIX <ssspinni@gmail.com>

--- carbon.mk
+++ carbon.mk
@@ -18,10 +18,10 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
 # Inherit some common Carbon stuff.
-$(call inherit-product, vendor/carbon/config/common_phone.mk)
+$(call inherit-product, vendor/stelix/config/common_phone.mk)
 
 # Enhanced NFC
-#$(call inherit-product, vendor/carbon/config/nfc_enhanced.mk)
+#$(call inherit-product, vendor/stelix/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/hammerhead/full_hammerhead.mk)
